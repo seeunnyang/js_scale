8d107f9aee58d35a3e83412d63a905876bf4f583
seeun <choise0804@naver.com>
2023-07-31 12:28:45 +0900

    0731_3

--- script/scale.js
+++ script/scale.js
@@ -3,12 +3,11 @@ let userHeight = document.querySelector('#user_height'); //키입력
 let userWeight = document.querySelector('#user_weight'); //몸무게입력
 const button = document.querySelector('#button'); //확인버튼
 let result = document.querySelector('#result_screen') //결과창
-let normal_w = 0
+let normal_w = (userHeight-100)*0.9
 //변수확인
 console.log(userHeight, userWeight, button, result, normal_w);
-//키입력후 확인버튼 눌렀을 때 결과창에 적정몸무게((본인신장-100)*0.9) 와 초과몸무게 나타내기
+//키입력후 확인버튼 클릭했을 때 결과창에 적정몸무게((본인신장-100)*0.9) 와 초과몸무게 나타내기
+//결과 => 적중체중은 ?kg 이며, ?kg 초과되셨습니다.
 button.addEventListener('click',function(){
-    //결과 => 적중체중은 ?kg 이며, ?kg 초과되셨습니다.
-    normal_w = (userHeight-100)*0.9 //대입연산자
-    result.innerHTML=(`적중체중은 ${normal_w}kg 이며, ${userWeight-normal_w}kg 초과 되셨습니다.`)
+    result.innerHTML = `적중체중은 ${normal_w}kg 이며, ${userWeight-normal_w}kg 입니다`
 })
